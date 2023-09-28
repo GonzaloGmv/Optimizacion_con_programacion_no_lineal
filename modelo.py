9f7ea2a5e19a811258004f0cd85d78c1f4a0ea0c
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2023-09-28 17:57:05 +0200

    commit

--- modelo.py
+++ modelo.py
@@ -13,17 +13,17 @@ betas = np.array([4533, 5833, 3333])
 x = np.linspace(1, TOTAL_BUDGET, TOTAL_BUDGET)
 
 # Variables
-google   = cp.Variable(pos=True)
-facebook = cp.Variable(pos=True)
-twitter  = cp.Variable(pos=True)
+hamburguesa   = cp.Variable(pos=True)
+patatas = cp.Variable(pos=True)
+croissants  = cp.Variable(pos=True)
 
 # Constraint
-constraint = [google + facebook + twitter <= TOTAL_BUDGET]
+constraint = [hamburguesa + patatas + croissants <= TOTAL_BUDGET]
 
 # Objective
-obj = cp.Maximize(alphas[0] + betas[0] * cp.log(google)
-                + alphas[1] + betas[1] * cp.log(facebook)
-                + alphas[2] + betas[2] * cp.log(twitter))
+obj = cp.Maximize(alphas[0] + betas[0] * cp.log(hamburguesa)
+                + alphas[1] + betas[1] * cp.log(patatas)
+                + alphas[2] + betas[2] * cp.log(croissants))
 
 # Solve
 prob = cp.Problem(obj, constraint)
@@ -34,21 +34,21 @@ print('='*59 + '\n' + ' '*24 + 'Solution' + ' '*24 + '\n' + '='*59)
 print(f'Status = {prob.status}')
 print(f'Returns = ${round(prob.value):,}\n')
 print('Hamburguesería Marketing allocation:')
-print(f' - Anuncios de Hamburguesa   = ${round(google.value):,}')
-print(f' - Anuncios de Patatas = ${round(facebook.value):,}')
-print(f' - Anuncios de Croissants  = ${round(twitter.value):,}')
+print(f' - Anuncios de Hamburguesa   = ${round(hamburguesa.value):,}')
+print(f' - Anuncios de Patatas = ${round(patatas.value):,}')
+print(f' - Anuncios de Croissants  = ${round(croissants.value):,}')
 
 # Plot the functions and the results
 fig = plt.figure(figsize=(10, 5), dpi=300)
-plt.plot(x, alphas[0] + betas[0] * np.log(x), color='red', label='Google Ads')
-plt.plot(x, alphas[1] + betas[1] * np.log(x), color='blue', label='Facebook Ads')
-plt.plot(x, alphas[2] + betas[2] * np.log(x), color='green', label='Twitter Ads')
+plt.plot(x, alphas[0] + betas[0] * np.log(x), color='red', label='Anuncios de Hamburguesa')
+plt.plot(x, alphas[1] + betas[1] * np.log(x), color='blue', label='Anuncios de Patatas')
+plt.plot(x, alphas[2] + betas[2] * np.log(x), color='green', label='Anuncios de Croissants')
 
 # Plot optimal points
-plt.scatter([google.value, facebook.value, twitter.value],
-            [alphas[0] + betas[0] * np.log(google.value),
-             alphas[1] + betas[1] * np.log(facebook.value),
-             alphas[2] + betas[2] * np.log(twitter.value)],
+plt.scatter([hamburguesa.value, patatas.value, croissants.value],
+            [alphas[0] + betas[0] * np.log(hamburguesa.value),
+             alphas[1] + betas[1] * np.log(patatas.value),
+             alphas[2] + betas[2] * np.log(croissants.value)],
              marker="+", color='black', zorder=10)
 
 plt.xlabel('Budget ($)')
